53ed734ccc2d09fe9a36468a02b220ca27062c17
PedroCollin <pedrocollintkd@gmail.com>
2021-09-21 19:22:21 +0000

    hihi

--- AppToAdd/src/views/Photo/Photo.js
+++ AppToAdd/src/views/Photo/Photo.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {TouchableOpacity, Text, Button, Image, View, ImagePickerIOS} from 'react-native';
+import {TouchableOpacity, Text, Button, Image, View} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import ImagePicker from "react-native-image-picker"
 
 
 export default class Photo extends React.Component {
@@ -12,32 +13,33 @@ export default class Photo extends React.Component {
         ImagePicker.launchImageLibrary(options, response => {
             console.log("response", response);
             if(response.uri){
-                this.setState({ photo: response })
+                this.setState({ photo: response });
             }
        });
     };
 
-    voltar = () => {
-        navigation.navigate('Inicial')
-    };
+
 
     render() {
-    return (
-        <View>
-        <TouchableOpacity onPress={voltar}>       
-        <Text>Voltar</Text>  
-        </TouchableOpacity>
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        {photo &&(
-            <Image 
-                source={{ uri: photo.uri }}
-                style={{ width: 300, height:300 }}
-            />
-        )}
-            <Button title="Escolha uma foto" onPress={this.handleChoosePhoto} />
-        </View>
-        </View>
-    ); 
-}
-
+        const voltar = () => {
+            navigation.navigate('Inicial')
+        };
+        const photo = this.state;
+        return (
+            <View>
+            <TouchableOpacity onPress={voltar}>       
+            <Text>Voltar</Text>  
+            </TouchableOpacity>
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            {photo &&(
+                <Image 
+                    source={{ uri: photo.uri }}
+                    style={{ width: 300, height:300 }}
+                />
+            )}
+                <Button title="Escolha uma foto" onPress={this.handleChoosePhoto} />
+            </View>
+            </View>
+        ); 
+    }
 }
