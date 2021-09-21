d8bd11d36349a4735feba0c8e73976f8c6fe192d
PedroCollin <pedrocollintkd@gmail.com>
2021-09-21 23:43:02 +0000

    af

--- AppToAdd/src/views/Photo/Photo.js
+++ AppToAdd/src/views/Photo/Photo.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {TouchableOpacity, Text, Button, Image, View} from 'react-native';
+import {TouchableOpacity, Text, Button, ImageBackground, View} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import ImagePicker from "react-native-image-picker"
+import * as ImagePicker from 'react-native-image-picker'
 
 
 export default class Photo extends React.Component {
@@ -21,24 +21,18 @@ export default class Photo extends React.Component {
 
 
     render() {
-        const voltar = ({}) => {
-            navigation.navigate('Inicial')
-        };
         const photo = this.state;
         return (
             <View>
-            <TouchableOpacity onPress={voltar}>       
-            <Text>Voltar</Text>  
-            </TouchableOpacity>
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            {photo &&(
-                <Image 
-                    source={{ uri: photo.uri }}
-                    style={{ width: 300, height:300 }}
-                />
-            )}
-                <Button title="Escolha uma foto" onPress={this.handleChoosePhoto} />
-            </View>
+                <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                {photo &&(
+                    <ImageBackground 
+                        source={{ uri: photo.uri }}
+                        style={{ width: 300, height:300 }}
+                    />
+                )}
+                    <Button title="Escolha uma foto" onPress={this.handleChoosePhoto} />
+                </View>
             </View>
         ); 
     }
