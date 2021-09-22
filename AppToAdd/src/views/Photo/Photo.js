2c9939ffd0c12aca043ea40fbaec5e1b60a0b441
PedroCollin <pedrocollintkd@gmail.com>
2021-09-22 00:28:15 +0000

    a

--- AppToAdd/src/views/Photo/Photo.js
+++ AppToAdd/src/views/Photo/Photo.js
@@ -1,39 +1,37 @@
 import React from 'react';
-import {TouchableOpacity, Text, Button, ImageBackground, View} from 'react-native';
+import {TouchableOpacity, Text, Button, Image, View} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import * as ImagePicker from 'react-native-image-picker'
+import ImagePicker from 'react-native-image-picker';
 
+const Photo = () => {
 
-export default class Photo extends React.Component {
-
-    handleChoosePhoto = () => {
-        const options = {
-            noData: true
-        };
-        ImagePicker.launchImageLibrary(options, response => {
-            console.log("response", response);
-            if(response.uri){
-                this.setState({ photo: response });
-            }
-       });
+    const options = {
+        title: 'Pick an image',
+        storageOptions: {
+            skipBackup: true,
+            path: 'images',
+        },
     };
 
+    const openPicker = () => {
+        ImagePicker.showImagePicker(options, (response) => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error:', response.error);
+            } else {
+                const source = {uri: response.uri};
+            }
+        });
+    };
 
-
-    render() {
-        const photo = this.state;
-        return (
-            <View>
-                <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                {photo &&(
-                    <ImageBackground 
-                        source={{ uri: photo.uri }}
-                        style={{ width: 300, height:300 }}
-                    />
-                )}
-                    <Button title="Escolha uma foto" onPress={this.handleChoosePhoto} />
-                </View>
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent:'center' }}>
+            <Button title="Pick image" onPress={openPicker}
+            />     
             </View>
-        ); 
-    }
+    )
+
 }
+
+export default Photo;
